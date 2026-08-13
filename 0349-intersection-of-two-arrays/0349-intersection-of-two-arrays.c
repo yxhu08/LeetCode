f799c327da7bcc8d3e0d55bed9811fefdde6eaea
Yashvi Bansal <223410059+yxhu08@users.noreply.github.com>
2026-08-13 14:46:24 +0530

    Time: 0 ms (100%), Space: 11.5 MB (14.44%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.c
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.c
@@ -1,9 +1,5 @@
 #include <stdlib.h>
 
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
@@ -11,36 +7,29 @@ int* intersection(int* nums1, int nums1Size,
                   int* nums2, int nums2Size,
                   int* returnSize) {
 
-    qsort(nums1, nums1Size, sizeof(int), compare);
-    qsort(nums2, nums2Size, sizeof(int), compare);
+    // Acts like a hash set
+    int hash[1001] = {0};
 
-    int maxSize = nums1Size < nums2Size ? nums1Size : nums2Size;
+    // Store elements of nums1
+    for (int i = 0; i < nums1Size; i++) {
+        hash[nums1[i]] = 1;
+    }
 
-    int* result = malloc(sizeof(int) * maxSize);
+    // Maximum possible result size
+    int* result = malloc(sizeof(int) * 1001);
 
-    int i = 0;
-    int j = 0;
     int k = 0;
 
-    while (i < nums1Size && j < nums2Size) {
+    // Check elements of nums2
+    for (int i = 0; i < nums2Size; i++) {
 
-        if (nums1[i] == nums2[j]) {
+        if (hash[nums2[i]] == 1) {
 
-            if (k == 0 || result[k - 1] != nums1[i]) {
-                result[k] = nums1[i];
-                k++;
-            }
+            result[k] = nums2[i];
+            k++;
 
-            i++;
-            j++;
-        }
-
-        else if (nums1[i] < nums2[j]) {
-            i++;
-        }
-
-        else {
-            j++;
+            // Set to 0 so duplicate isn't added
+            hash[nums2[i]] = 0;
         }
     }
 
